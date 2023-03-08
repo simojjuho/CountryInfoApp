d798649800c5e7bd4495948f62fbdb6ec8dcec18
Juho Simojoki <juho.simojoki@metropolia.fi>
2023-03-08 12:41:23 +0200

    Removed the old filterReducer file, that's not needed.

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countryReducer from "./reducers/countryReducer";
-import filterReducer from "./reducers/filterReducer";
 
 const store = configureStore({
   reducer: {
     countries: countryReducer,
-    filter: filterReducer,
   },
 });
 
